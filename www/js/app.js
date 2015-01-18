d95b996677c4537730d1fb87285a0b7fafd2d4e5
filipesilva <filipematossilva@gmail.com>
2015-01-18 12:09:11 +0000

    added geolocation plugin

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('save-a-selfie', ['ionic', 'uiGmapgoogle-maps',
+angular.module('save-a-selfie', ['ionic', 'uiGmapgoogle-maps', 'ngCordova',
   'save-a-selfie.controllers', 'save-a-selfie.services'
 ])
 
