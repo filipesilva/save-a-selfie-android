988ffcc62be80eff4459fb1f6daf3d759841ca2e
filipesilva <filipematossilva@gmail.com>
2015-01-18 15:13:11 +0000

    added user location on map

--- www/js/controllers/locate.js
+++ www/js/controllers/locate.js
@@ -1,4 +1,45 @@
 angular.module('save-a-selfie.controllers')
-  .controller('LocateCtrl', function() {
-    this.map = { center: { latitude: 53, longitude: -5 }, zoom: 5 };
+  .controller('LocateCtrl', function($rootScope, $cordovaGeolocation) {
+    var view = this;
+    view.map = {
+      zoom: 16
+    };
+
+    // TODO handle missing geolocation permission, loading
+    this.resolve = function() {
+      $cordovaGeolocation
+        .getCurrentPosition({
+          timeout: 10000,
+          enableHighAccuracy: false
+        })
+        .then(function(position) {
+          view.map.center = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          };
+          view.map.user = {
+            id: 'user',
+            options: {
+              icon: {
+                anchor: {
+                  x: 24,
+                  y: 24
+                },
+                url: 'img/user-location-marker.png'
+              }
+            },
+            coords : {
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude
+            }
+          };
+        }, function(err) {
+          // error
+        });
+
+    };
+
+    $rootScope.$on("$ionicView.enter", function(scopes, states) {
+      view.resolve();
+    });
   });
