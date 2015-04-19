d6875a81077c16739f42ed9b29010c488b87f519
filipesilva <filipematossilva@gmail.com>
2015-04-19 19:37:10 +0100

    updated ionic project

--- www/app/app.constants.js
+++ www/app/app.constants.js
@@ -1,11 +1,11 @@
 (function() {
   'use strict';
-  
+
   angular
     .module('save-a-selfie')
     .value('Camera', null)
     .constant('apiUrl', '/proxy');
   // TODO set this url for production, as right now the ionic proxy
   // is managing the connection to prevent CORS errors
-  // .constant('apiUrl', http://www.saveaselfie.org');
+  // .constant('apiUrl', 'http://www.saveaselfie.org');
 })();
